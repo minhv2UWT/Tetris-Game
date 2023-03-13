d11ddede106323447eb8d27dbdd53d389a65b7f1
kojiyoshiyama <kojiyo@uw.edu>
2023-03-12 18:25:31 -0700

    fixed all PMD and checkstyle

--- src/view/NextPiece.java
+++ src/view/NextPiece.java
@@ -1,18 +1,45 @@
+/*
+ * TCSS 305 – Winter 2023
+ * Group Project – Tetris
+ */
+
 package view;
-
-import model.*;
-import model.Point;
-
-import javax.swing.*;
-import java.awt.*;
-
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
 
 import static model.CreateBoard.PROPERTY_NEXT_PIECE;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.io.Serial;
+import javax.swing.JPanel;
+import model.Block;
+import model.Point;
+import model.TetrisPiece;
+
+/**
+ * This class creates the game board where Tetris is played.
+ * @author Koji Yoshiyama
+ * @author Minh Vu
+ * @author Simran Narwal
+ * @author Jack Chen
+ * @version Winter 2023
+ */
 
 public class NextPiece extends JPanel implements PropertyChangeListener {
+
+    /**
+     * This is the serial version UID for the class.
+     */
+    @Serial
+    private static final long serialVersionUID = 123456789L;
+
+    /**
+     * Number of rows in the game board.
+     */
     private static final int ROWS = 5;
 
     /**
@@ -24,83 +51,104 @@ public class NextPiece extends JPanel implements PropertyChangeListener {
      * The width of each block in pixels.
      */
     private static final int BLOCK_SIZE = 40;
+
+    /**
+     * This is the piece to be drawn.
+     */
     private TetrisPiece myNextPiece;
 
-
+    /**
+     * This method is a constructor for the nextPiece class.
+     */
     public NextPiece() {
-
+        super();
         setBackground(Color.BLACK);
         setPreferredSize(new Dimension(COLUMNS * BLOCK_SIZE, ROWS * BLOCK_SIZE));
-
-
     }
 
+    /**
+     * This method looks out for property changes within the model
+     * so the next piece panel can be updated.
+     * @param theEvt A PropertyChangeEvent object describing the event source
+     *          and the property that has changed.
+     */
     @Override
-    public void propertyChange(PropertyChangeEvent evt) {
-        if (PROPERTY_NEXT_PIECE.equals(evt.getPropertyName())) {
-            myNextPiece = (TetrisPiece) evt.getNewValue();
+    public void propertyChange(final PropertyChangeEvent theEvt) {
+        if (PROPERTY_NEXT_PIECE.equals(theEvt.getPropertyName())) {
+            myNextPiece = (TetrisPiece) theEvt.getNewValue();
             repaint();
         }
 
     }
 
-
+    /**
+     * This method is called by propertyChange, and it repaints the nextPiece panel.
+     * @param theGraphics the <code>Graphics</code> object to protect
+     */
     @Override
-    protected void paintComponent(Graphics theGraphics) {
+    protected void paintComponent(final Graphics theGraphics) {
         super.paintComponent(theGraphics);
         final Graphics2D g2d = (Graphics2D) theGraphics;
-        // for better graphics display
-        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-                RenderingHints.VALUE_ANTIALIAS_ON);
+        setRenderingHints(g2d);
         if (myNextPiece != null) {
             drawNextPiece(g2d);
         }
         drawGrid(g2d);
     }
 
-    private void drawNextPiece(Graphics2D g2d) {
-        for (Point p : myNextPiece.getPoints()) {
-            g2d.setColor(getColor(myNextPiece.getBlock()));
-            g2d.fillRect((p.x() + 1) * BLOCK_SIZE, (p.y()+1) * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
+    /**
+     * This private helper method was made to satisfy the law of
+     * demeter and makes the graphics sharper.
+     * @param theGraphics the current graphic that is being drawn.
+     */
+    private void setRenderingHints(final Graphics2D theGraphics) {
+        theGraphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+    }
+
+    /**
+     * This helper method is called by paintComponent,
+     * and it is used to draw the next tetris piece.
+     * @param theG2d this is the graphic currently being drawn
+     */
+    private void drawNextPiece(final Graphics2D theG2d) {
+        for (final Point p : myNextPiece.getPoints()) {
+            theG2d.setColor(getColor(myNextPiece.getBlock()));
+            theG2d.fillRect((p.x() + 1) * BLOCK_SIZE, (p.y() + 1) * BLOCK_SIZE,
+                         BLOCK_SIZE, BLOCK_SIZE);
         }
     }
 
-    private void drawGrid(Graphics2D g2d) {
-        g2d.setColor(Color.RED);
+    /**
+     * This method is called by paintComponent, and it is used
+     * to draw the grid where the nextPiece is drawn.
+     * @param theG2d this is the graphic currently being drawn
+     */
+    private void drawGrid(final Graphics2D theG2d) {
+        theG2d.setColor(Color.RED);
         for (int i = 0; i <= ROWS; i++) {
             for (int j = 0; j <= COLUMNS; j++) {
-                g2d.drawLine(0, i * BLOCK_SIZE, COLUMNS * BLOCK_SIZE, i * BLOCK_SIZE);
-                g2d.drawLine(j * BLOCK_SIZE, 0, j * BLOCK_SIZE, ROWS * BLOCK_SIZE);
+                theG2d.drawLine(0, i * BLOCK_SIZE, COLUMNS * BLOCK_SIZE, i * BLOCK_SIZE);
+                theG2d.drawLine(j * BLOCK_SIZE, 0, j * BLOCK_SIZE, ROWS * BLOCK_SIZE);
             }
         }
     }
 
-    private Color getColor(Block block) {
-        Color blockColor = Color.BLACK;
-        switch (block) {
-            case I:
-                blockColor = Color.CYAN;
-                break;
-            case J:
-                blockColor = Color.BLUE;
-                break;
-            case L:
-                blockColor = Color.ORANGE;
-                break;
-            case O:
-                blockColor = Color.YELLOW;
-                break;
-            case S:
-                blockColor = Color.GREEN;
-                break;
-            case T:
-                blockColor = Color.decode("#A020F0"); // purple
-                break;
-            case Z:
-                blockColor = Color.RED;
-                break;
-        }
-        return blockColor;
+    /**
+     * This method finds out each piece's corresponding color.
+     * @param theBlock the piece sent in that will receive a color assignment.
+     * @return the color that the piece should be.
+     */
+    private Color getColor(final Block theBlock) {
+        return switch (theBlock) {
+            case I -> Color.CYAN;
+            case J -> Color.BLUE;
+            case L -> Color.ORANGE;
+            case O -> Color.YELLOW;
+            case S -> Color.GREEN;
+            case T -> Color.decode("#A020F0"); // purple
+            case Z -> Color.RED;
+        };
     }
 
 
--- src/view/TetrisGameBoard.java
+++ src/view/TetrisGameBoard.java
@@ -1,19 +1,43 @@
+/*
+ * TCSS 305 – Winter 2023
+ * Group Project – Tetris
+ */
+
 package view;
 
-import model.Block;
+import static model.CreateBoard.PROPERTY_CURRENT_PIECE;
+import static model.CreateBoard.PROPERTY_FROZEN_BLOCKS;
 
-import java.awt.*;
-
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.io.Serial;
 import java.util.LinkedList;
 import java.util.List;
-import javax.swing.*;
+import javax.swing.JPanel;
+import model.Block;
 
-import static model.CreateBoard.*;
-
+/**
+ * This class creates the game board where Tetris is played.
+ * @author Koji Yoshiyama
+ * @author Minh Vu
+ * @author Simran Narwal
+ * @author Jack Chen
+ * @version Winter 2023
+ */
 
 public class TetrisGameBoard extends JPanel implements PropertyChangeListener {
+
+    /**
+     * This is the serialVersionUID.
+     */
+    @Serial
+    private static final long serialVersionUID = 123456789L;
+
 
     /**
      * Number of rows.
@@ -26,92 +50,114 @@ public class TetrisGameBoard extends JPanel implements PropertyChangeListener {
     private static final int COLUMNS = 10;
 
     /**
-     * Size of a Unit
+     * Size of a grid cell.
      */
     private static final int UNIT = 28;
+
     /**
-     * my Movable Piece
+     * My movable Piece.
      */
     private List<Block[]> myBlocksOnBoard;
 
-
+    /**
+     * This method is the constructor for the TetrisGameBoard class.
+     */
     public TetrisGameBoard() {
+        super();
         setBackground(Color.BLACK);
         setPreferredSize(new Dimension(COLUMNS * UNIT, ROWS * UNIT));
-        myBlocksOnBoard = new LinkedList<Block[]>();
+        myBlocksOnBoard = new LinkedList<>();
     }
 
+    /**
+     * This method receives the property changes in the model so the
+     * game board can update accordingly.
+     *
+     * @param theEvt A PropertyChangeEvent object describing the event source
+     *          and the property that has changed.
+     */
     @Override
-    public void propertyChange(PropertyChangeEvent evt) {
-        if (PROPERTY_CURRENT_PIECE.equals(evt.getPropertyName())) {
-            myBlocksOnBoard = (List<Block[]>) evt.getNewValue();
+    public void propertyChange(final PropertyChangeEvent theEvt) {
+        if (PROPERTY_CURRENT_PIECE.equals(theEvt.getPropertyName())) {
+            myBlocksOnBoard = (List<Block[]>) theEvt.getNewValue();
             repaint();
         }
-        if (PROPERTY_FROZEN_BLOCKS.equals(evt.getPropertyName())) {
-            myBlocksOnBoard = (List<Block[]>) evt.getNewValue();
+        if (PROPERTY_FROZEN_BLOCKS.equals(theEvt.getPropertyName())) {
+            myBlocksOnBoard = (List<Block[]>) theEvt.getNewValue();
             repaint();
         }
     }
 
+    /**
+     * This method, called by propertyChange, repaints the game board
+     * which includes the pieces and the grid.
+     * @param theGraphics the <code>Graphics</code> object to protect
+     */
     @Override
-    protected void paintComponent(Graphics theGraphics) {
+    protected void paintComponent(final Graphics theGraphics) {
         super.paintComponent(theGraphics);
         final Graphics2D g2d = (Graphics2D) theGraphics;
-        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-                RenderingHints.VALUE_ANTIALIAS_ON);
-
+        setRenderingHints(g2d);
         drawUpdatedBoard(g2d);
         drawGrid(g2d);
     }
 
-    private void drawGrid(Graphics2D g2d) {
-        g2d.setColor(Color.WHITE);
+    /**
+     * This private helper method was made to satisfy the law of
+     * demeter and makes the graphics sharper.
+     * @param theGraphics the current graphic that is being drawn.
+     */
+    private void setRenderingHints(final Graphics2D theGraphics) {
+        theGraphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                                     RenderingHints.VALUE_ANTIALIAS_ON);
+    }
+
+    /**
+     * This is a helper method that is called in paintComponent that draws the grid.
+     * @param theG2d this is the graphic that is being drawn.
+     */
+    private void drawGrid(final Graphics2D theG2d) {
+        theG2d.setColor(Color.WHITE);
         for (int i = 0; i <= ROWS; i++) {
             for (int column = 0; column <= COLUMNS; column++) {
-                g2d.drawLine(0, i * UNIT, COLUMNS * UNIT, i * UNIT);
-                g2d.drawLine(column * UNIT, 0, column * UNIT, ROWS * UNIT);
+                theG2d.drawLine(0, i * UNIT, COLUMNS * UNIT, i * UNIT);
+                theG2d.drawLine(column * UNIT, 0, column * UNIT, ROWS * UNIT);
             }
         }
     }
 
-    private void drawUpdatedBoard(Graphics2D g2d) {
+    /**
+     * This is a helper method that is called in paintComponent that draws the pieces.
+     * @param theG2d this is the graphic that is being drawn.
+     */
+    private void drawUpdatedBoard(final Graphics2D theG2d) {
         for (int row = 0; row < myBlocksOnBoard.size(); row++) {
-            for (int column = 0; column < myBlocksOnBoard.get(row).length; column++) {
+            final Block[] rowBlocks = myBlocksOnBoard.get(row);
+            for (int column = 0; column < rowBlocks.length; column++) {
                 if (myBlocksOnBoard.get(row)[column] != null) {
-                    g2d.setColor(getColor(myBlocksOnBoard.get(row)[column]));
-                    int upsideDown = ROWS - (1 + row);
-                    g2d.fillRect(column * UNIT, upsideDown * UNIT, UNIT, UNIT);
+                    theG2d.setColor(getColor(myBlocksOnBoard.get(row)[column]));
+                    final int upsideDown = ROWS - (1 + row);
+                    theG2d.fillRect(column * UNIT, upsideDown * UNIT, UNIT, UNIT);
                 }
             }
         }
     }
 
-    private Color getColor(Block block) {
-        Color blockColor = Color.BLACK;
-        switch (block) {
-            case I:
-                blockColor = Color.CYAN;
-                break;
-            case J:
-                blockColor = Color.BLUE;
-                break;
-            case L:
-                blockColor = Color.ORANGE;
-                break;
-            case O:
-                blockColor = Color.YELLOW;
-                break;
-            case S:
-                blockColor = Color.GREEN;
-                break;
-            case T:
-                blockColor = Color.decode("#A020F0"); // purple
-                break;
-            case Z:
-                blockColor = Color.RED;
-                break;
-        }
-        return blockColor;
+    /**
+     * This method finds out each piece's corresponding color.
+     * @param theBlock the piece sent in that will receive a color assignment.
+     * @return the color that the piece should be.
+     */
+    private Color getColor(final Block theBlock) {
+        return switch (theBlock) {
+            case I -> Color.CYAN;
+            case J -> Color.BLUE;
+            case L -> Color.ORANGE;
+            case O -> Color.YELLOW;
+            case S -> Color.GREEN;
+            case T -> Color.decode("#A020F0"); // purple
+            case Z -> Color.RED;
+        };
     }
 }
 
